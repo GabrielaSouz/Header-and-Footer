8e8fc515ffb2baf6fd989986b9d16fb8bfef60a3
GabrielaSouz <souza.gab@hotmail.com>
2025-06-20 11:19:58 +0800

    pathname

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import Link from 'next/link';
 import { DialogTitle } from './ui/dialog';
+import { usePathname } from 'next/navigation';
 
 const navItems = [
   { label: 'Início', href: '/' },
@@ -14,6 +15,8 @@ const navItems = [
 ];
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="w-full border-b bg-white sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
@@ -23,15 +26,19 @@ export default function Header() {
 
         {/* Desktop Nav */}
         <nav className="hidden md:flex space-x-6">
-          {navItems.map((item) => (
+          {navItems.map((item) => {
+            const isActive = pathname === item.href;
+            return (
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm font-medium text-gray-600 hover:text-black transition-colors"
+              className={`text-sm font-medium transition-colors ${
+                isActive ? 'text-pink-400' : 'text-gray-700 hover:text-pink-400'
+              }`}
             >
               {item.label}
             </Link>
-          ))}
+          );})}
         </nav>
 
         {/* Mobile Nav */}
@@ -47,15 +54,19 @@ export default function Header() {
               <DialogTitle className="sr-only">Menu de navegação</DialogTitle>
 
               <div className="flex flex-col justify-center items-center gap-4 mt-6">
-                {navItems.map((item) => (
+                {navItems.map((item) => {
+const isActive = pathname === item.href;
+return (
                   <Link
                     key={item.href}
                     href={item.href}
-                    className="text-sm font-medium text-gray-700 hover:text-black transition-colors"
-                  >
+                  className={`text-sm font-medium transition-colors ${
+                isActive ? 'text-pink-400' : 'text-gray-700 hover:text-pink-400'
+              }`}
+            >
                     {item.label}
                   </Link>
-                ))}
+                );})}
               </div>
             </SheetContent>
           </Sheet>
